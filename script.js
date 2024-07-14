4b65d66ccb850293ac9615806c2513f01a384d03
DHARM <opdsbanasya@gmail.com>
2024-07-14 23:49:12 +0530

    page 3 gooey effect created

--- script.js
+++ script.js
@@ -81,9 +81,9 @@ const loadingAnimation = () => {
     })
     tl.from("#hero1, page2", {
         opacity: 0,
-    },"-=1.2")
+    }, "-=1.2")
 }
-loadingAnimation();
+// loadingAnimation();
 
 
 const cursurAnimation = () => {
@@ -107,3 +107,14 @@ gsap.from(".underline", {
         start: "top 90%",
     }
 })
+
+const sheryAnimation = () => {
+    Shery.imageEffect(".image-div", {
+        style: 6,
+        // debug: true,
+        config: { "noiseDetail": { "value": 8.4, "range": [0, 100] }, "distortionAmount": { "value": 3.44, "range": [0, 10] }, "scale": { "value": 36.36, "range": [0, 100] }, "speed": { "value": 1, "range": [0, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272695760684946 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": false }, "maskVal": { "value": 1.15, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.76, "range": [0, 10] }, "metaball": { "value": 0.41, "range": [0, 2] }, "discard_threshold": { "value": 0.6, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.44, "range": [0, 2] }, "noise_scale": { "value": 16.03, "range": [0, 100] } },
+        
+        gooey: true,
+    });
+}
+sheryAnimation();
